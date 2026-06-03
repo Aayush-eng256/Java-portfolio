ed6f64aaed849af583ad3477621002f84649c10f
Ayush Ranjan <aayushranjan256@gmail.com>
2026-06-03 19:43:15 +0530

    Adding character paterns in java

---
+++ PatterPrinting/CharacterPatter.java
@@ -0,0 +1,13 @@
+public class CharacterPatter {
+    public static void main(String[] args) {
+        int n = 4;
+        char ch = 'A';
+        for(int lines = 1;lines<=4;lines++) {
+            for(int chars = 1;chars<=lines;chars++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
--- PatterPrinting/Star.java
+++ PatterPrinting/Star.java
@@ -2,7 +2,7 @@ public class Star {
     public static void main(String args[]) {
         for(int lines = 1;lines<=4;lines++) {
             for(int stars = 1;stars<=lines;stars++) {
-                System.out.print("*");
+                System.out.print(stars);
             }
             System.out.println();
         }
