426b8b5cdf58594edd33e13a47c900e894e1f8f1
Ayush Ranjan <aayushranjan256@gmail.com>
2026-06-10 19:09:47 +0530

    Adding Recursion  Questions

---
+++ Java-Practise/Recursion/isSorted.java
@@ -0,0 +1,16 @@
+public class isSorted {
+    public static boolean isSorted(int arr[],int i) {
+        if(i == arr.length) {
+            return true;
+        }
+        if(arr[i] < arr[i+1]) {
+            return true;
+        }
+          return isSorted(arr, i+1);
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int i = 0;
+        System.out.print(isSorted(arr,i));
+    }
+}
